9017456cd711d2c151d12df229c3acf26b5b378c
Karol <kpulawski13@gmail.com>
2023-10-14 11:34:24 +0100

    initial commit;

---
+++ src/FavoriteDish.jsx
@@ -0,0 +1,9 @@
+function FavoriteDish() {
+  return <h1>My favorite dish is Hashbrowns</h1>;
+}
+function FavoriteDish2() {
+  return <h1>My second best dish is spaghetti</h1>;
+}
+
+export let Dish = FavoriteDish,
+  Dish2 = FavoriteDish2;
---
+++ src/Greeting.jsx
@@ -0,0 +1,5 @@
+function Greeting() {
+  return <h1> &quot;Tread softly because you tread on my dreams.&quot;</h1>;
+}
+
+export default Greeting;
