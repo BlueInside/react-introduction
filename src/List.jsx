4e20d213e9dafb89c6dd91601ea37c2a0ce86f4f
Karol <kpulawski13@gmail.com>
2023-10-17 10:47:09 +0100

    rendering list practice

---
+++ src/List.jsx
@@ -0,0 +1,13 @@
+const people = [
+  'Creola Katherine Johnson: mathematician',
+  'Mario José Molina-Pasquel Henríquez: chemist',
+  'Mohammad Abdus Salam: physicist',
+  'Percy Lavon Julian: chemist',
+  'Subrahmanyan Chandrasekhar: astrophysicist',
+];
+
+const listItems = people.map((person) => <li>{person}</li>);
+
+export default function List() {
+  return <ul>{listItems}</ul>;
+}
--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import Avatar from './Avatar';
+import List from './List';
 
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Avatar></Avatar>
+    <List></List>
   </React.StrictMode>
 );
