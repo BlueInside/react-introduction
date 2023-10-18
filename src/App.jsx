3ea497ecae81de76c92a31d9468814b0f8f78559
Karol <kpulawski13@gmail.com>
2023-10-18 17:07:35 +0100

    props practice

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,69 @@
-import './App.css';
+import { getImageUrl } from './utils.js';
 
-function App() {
+const scientists = [
+  {
+    name: 'Maria Skłodowska-Curie',
+    imageUrl: 'szV5sdG',
+    profession: 'physicist and chemist',
+    awards: [
+      'Nobel Prize in Physics',
+      'Nobel Prize in Chemistry',
+      'Davy Medal',
+      'Matteucci Medal',
+    ],
+    discovered: 'polonium (chemical element)',
+  },
+  {
+    name: 'Katsuko Saruhashi',
+    imageUrl: 'YfeOqp2',
+    profession: 'geochemist',
+    awards: ['Miyake Prize for geochemistry', 'Tanaka Prize'],
+    discovered: 'a method for measuring carbon dioxide in seawater',
+  },
+];
+
+function Profile({ fullname, size, imageUrl, profession, awards, discovered }) {
   return (
-    <>
-      <h1>Hello World</h1>
-    </>
+    <section className="profile">
+      <h2>{fullname}</h2>
+      <img
+        className="avatar"
+        src={getImageUrl(`${imageUrl}`)}
+        alt={`${fullname}`}
+        width={size}
+        height={size}
+      />
+      <ul>
+        <li>
+          <b>Profession: </b>
+          {profession}
+        </li>
+        <li>
+          <b>Awards: {awards.length} </b>
+          {awards.map((award, index) => (
+            <span key={index}>{award}, </span>
+          ))}
+        </li>
+        <li>
+          <b>Discovered: </b>
+          {discovered}
+        </li>
+      </ul>
+    </section>
   );
 }
 
-export default App;
+Profile.defaultProps = {
+  size: 70,
+};
+
+export default function Gallery() {
+  return (
+    <div>
+      <h1>Notable Scientists</h1>
+      {scientists.map((scientist) => (
+        <Profile key={scientist.fullname} {...scientist} />
+      ))}
+    </div>
+  );
+}
--- src/List.jsx
+++ src/List.jsx
@@ -1,13 +1,2 @@
-const people = [
-  'Creola Katherine Johnson: mathematician',
-  'Mario José Molina-Pasquel Henríquez: chemist',
-  'Mohammad Abdus Salam: physicist',
-  'Percy Lavon Julian: chemist',
-  'Subrahmanyan Chandrasekhar: astrophysicist',
-];
 
-const listItems = people.map((person) => <li>{person}</li>);
 
-export default function List() {
-  return <ul>{listItems}</ul>;
-}
